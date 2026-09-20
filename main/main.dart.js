f267921cd56a8ee87d931f1f766885249f71d9a3
allapcallapc <allapcallapc@users.noreply.github.com>
2026-09-20 01:48:39 +0000

    deploy: main @ cbf60f2cb04f79a54ec508ba6a7e0d4e17c6b814 cbf60f2cb04f79a54ec508ba6a7e0d4e17c6b814

--- main/main.dart.js
+++ main/main.dart.js
@@ -37239,7 +37239,7 @@ $S:3}
 A.b_2.prototype={
 $1(a){var s=A.dW().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/06a2e2a110089dff50fe635cffd2a61e1b24fbcd/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/af7e796e161ae0bb1ff0758c71a7105418bd9ded/":s)+a},
 $S:36}
 A.XO.prototype={
 gv(a){var s=this.a
